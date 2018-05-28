ca1afe3cea5eeab40607dde4b6dd53d9b2de4d0f
Justin Lane <justin@justinlane.org>
2018-05-28 15:54:43 +1200

    Remove Unneeded Code

--- src/Components/Messages/ResumeTitleMessages.js
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl'
-
-export default defineMessages({
-  default: {
-    id: 'rego.components.ResumeTitle.default',
-    defaultMessage: 'Your Message Here!'
-  }
-})
