cf438c186c07634c8d65d9b331952cb6f45e41da
Justin Lane <justin@justinlane.org>
2018-06-12 14:51:23 +1200

    Refactor Experience Card Styles

--- src/Components/ExperienceCard.js
+++ src/Components/ExperienceCard.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardContent from '@material-ui/core/CardContent'
-import Typography from '@material-ui/core/Typography'
-import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
-import Chip from '@material-ui/core/Chip'
+import {
+  IconButton,
+  Chip,
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Avatar
+} from '@material-ui/core'
 import { ChevronDownIcon, ChevronUpIcon } from 'mdi-react'
 import { Images } from '../Themes'
+import styles from './Styles/ExperienceCardStyle'
 
 class ExperienceCard extends React.Component {
   constructor (props) {
@@ -21,104 +24,41 @@ class ExperienceCard extends React.Component {
 
   render () {
     return (
-      <div style={{ minWidth: 275 }}>
+      <div style={styles.container}>
         <Card>
           <CardHeader
             avatar={
               <Avatar aria-label='tiaki' alt='tiaki' src={Images.tiaki} />
             }
             title={
-              <div
-                style={{
-                  display: 'flex',
-                  flex: 1,
-                  flexDirection: 'row'
-                }}
-              >
-                <div
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    flex: 1
-                  }}
-                >
-                  <div
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      flex: 1,
-                      justifyContent: 'space-between',
-                      marginRight: 4
-                    }}
-                  >
-                    <div>Founder, tiaki.care</div>
-                    <div>2015 - now</div>
-                  </div>
-                  <IconButton onClick={this.handleExpandClick.bind(this)}>
-                    {this.state.expanded ? (
-                      <ChevronUpIcon />
-                    ) : (
-                      <ChevronDownIcon />
-                    )}
-                  </IconButton>
+              <div style={styles.titleWrapper}>
+                <div style={styles.title}>
+                  <div>Founder, tiaki.care</div>
+                  <div>2015 - now</div>
                 </div>
+                <IconButton onClick={this.handleExpandClick.bind(this)}>
+                  {this.state.expanded ? (
+                    <ChevronUpIcon />
+                  ) : (
+                    <ChevronDownIcon />
+                  )}
+                </IconButton>
               </div>
             }
           />
           <div>
             {this.state.expanded && (
-              <div style={{ marginTop: -20 }}>
+              <div style={styles.cardStyle}>
                 <CardContent>
                   <Typography paragraph>
                     creating a SaaS for early childhood centres
                   </Typography>
 
-                  <Chip
-                    label={'React Native'}
-                    style={{
-                      marginTop: 5,
-                      background: 'white',
-                      marginRight: 5,
-                      border: '1px solid black'
-                    }}
-                  />
-                  <Chip
-                    label={'React'}
-                    style={{
-                      marginTop: 5,
-                      background: 'white',
-                      marginRight: 5,
-                      border: '1px solid black'
-                    }}
-                  />
-                  <Chip
-                    label={'Elixir'}
-                    style={{
-                      marginTop: 5,
-                      background: 'white',
-                      marginRight: 5,
-                      border: '1px solid black'
-                    }}
-                  />
-                  <Chip
-                    label={'Ruby'}
-                    style={{
-                      marginTop: 5,
-                      background: 'white',
-                      marginRight: 5,
-                      border: '1px solid black'
-                    }}
-                  />
-                  <Chip
-                    label={'AWS'}
-                    style={{
-                      marginTop: 5,
-                      background: 'white',
-                      marginRight: 5,
-                      border: '1px solid black'
-                    }}
-                  />
+                  <Chip label={'React Native'} style={styles.chipStyle} />
+                  <Chip label={'React'} style={styles.chipStyle} />
+                  <Chip label={'Elixir'} style={styles.chipStyle} />
+                  <Chip label={'Ruby'} style={styles.chipStyle} />
+                  <Chip label={'AWS'} style={styles.chipStyle} />
                 </CardContent>
               </div>
             )}
--- src/Components/Styles/ExperienceCardStyle.js
+++ src/Components/Styles/ExperienceCardStyle.js
@@ -2,7 +2,29 @@
 
 const styles = {
   container: {
+    minWidth: 275
+  },
+  titleWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
     flex: 1
+  },
+  title: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginRight: 4
+  },
+  cardStyle: {
+    marginTop: -20
+  },
+  chipStyle: {
+    marginTop: 5,
+    background: 'white',
+    marginRight: 5,
+    border: '1px solid black'
   }
 }
 export default styles
