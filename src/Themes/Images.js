58c7b3eb826324b8a853ac9af08e1c4d680966d1
Justin Lane <justin@justinlane.org>
2018-06-19 17:29:15 +1200

    Remove References to Old Images :bug:

--- src/Themes/Images.js
+++ src/Themes/Images.js
@@ -1,10 +1,5 @@
 const images = {
   avatar: require('../Images/avatar.jpg'),
-  beer: require('../Images/beer.png'),
-  church: require('../Images/church.png'),
-  drink: require('../Images/hot-drink.png'),
-  laptop: require('../Images/laptop.png'),
-  shoes: require('../Images/shoes.png'),
   tiaki: require('../Images/tiaki-man.png'),
   kandid: require('../Images/kandid.png'),
   school: require('../Images/school.png')
